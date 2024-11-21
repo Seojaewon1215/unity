da659e32e7b1ca5e61dcbcbd00191c6657d65d33
Seojaewon1215 <sjcsc0@gmail.com>
2024-11-21 09:07:26 +0900

    (씬 조작기능[보스 입장 트리거 설정])

--- Assets/SJW/Undead Survivor/Sprites/Codes/Player.cs
+++ Assets/SJW/Undead Survivor/Sprites/Codes/Player.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -35,6 +36,21 @@ public class Player : MonoBehaviour
         anim.SetFloat("Speed", inputVec.magnitude);
         anim.SetFloat("Horizontal", inputVec.x);
         anim.SetFloat("Vertical", inputVec.y);
+
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            string currentScene = SceneManager.GetActiveScene().name;
+
+            // 디버깅 로그 추가: 현재 씬 확인
+            Debug.Log($"현재 씬: {currentScene}");
+
+            if (currentScene != "Top_down")
+            {
+                Debug.Log("Top_down 씬으로 이동합니다.");
+                SceneManager.LoadScene("Top_down");
+            }
+            
+        }
     }
     void Start()
     {
